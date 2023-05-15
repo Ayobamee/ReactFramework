61c6ec2313891d41147cdbc2559b283b04e65794
Ayobamee <ayobami.elutade@com>
2023-05-15 12:32:00 +0100

    push new changes

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,14 +4,10 @@ import classes from "./PostsList.module.css";
 import NewPost from "./Newpost";
 import Modal from "./Modal";
 
-function PostsList() {
-  const [modalIsVisble, setModalIsVisible] = useState(true);
+function PostsList({isPosting, onStopPosting}) {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
-  function hideModalHandler() {
-    setModalIsVisible(false);
-  }
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
   }
@@ -20,21 +16,29 @@ function PostsList() {
     setEnteredAuthor(event.target.value);
   }
 
-  let modalContent;
-  if (modalIsVisble) {
-    modalContent = (
-      <Modal onClose={hideModalHandler}>
-        <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={authorChangeHandler}
-        />
-      </Modal>
-    );
-  }
+  // let modalContent;
+  // if (modalIsVisble) {
+  //   modalContent = (
+  //     <Modal onClose={hideModalHandler}>
+  //       <NewPost
+  //         onBodyChange={bodyChangeHandler}
+  //         onAuthorChange={authorChangeHandler}
+  //       />
+  //     </Modal>
+  //   );
+  // }
 
   return (
     <>
-      {modalContent}
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Elutade" body="React cool sense die" />
